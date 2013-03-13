d726919fe4c2b9b5a0168c48d9119a1c076b6341
Leszek Rozniakowski <lrozniakowski@ioki.com.pl>
2013-03-13 14:45:09 +0100

    new methods

--- Twig/FunnyExtension.php
+++ Twig/FunnyExtension.php
@@ -5,9 +5,20 @@ namespace Gajdaw\TwigExtBundle\Twig;
 class FunnyExtension extends \Twig_Extension
 {
 
+    public function dotsFunction($number = 0)
+    {
+        return str_repeat('.', $number);
+    }
+
+    public function removespacesFilter($text)
+    {
+        return str_replace(' ', '', $text);
+    }
+
     public function getFilters()
     {
         return array(
+            'removespaces' => new \Twig_Filter_Method($this, 'removespacesFilter'),
             'funny' => new \Twig_Filter_Method($this, 'funnyFilter'),
         );
     }
@@ -20,6 +31,7 @@ class FunnyExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
+            'dots' => new \Twig_Function_Method($this, 'dotsFunction'),
             'smile' => new \Twig_Function_Method($this, 'smileFunction'),
         );
     }
